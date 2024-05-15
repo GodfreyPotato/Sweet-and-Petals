a52184c218f79b9fe8715844e9a4ff55b5f3811a
Jiri <128820948+GodfreyPotato@users.noreply.github.com>
2024-05-15 23:02:21 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+function hide(){
+        document.getElementById("phone").style.display = "none";
+
+
+}
+
+function exit(){
+    document.getElementById("phone").style.display = "none";
+}
+function show(){
+    document.getElementById("phone").style.display = "flex";
+}
+
+
+
+if(window.screen.width>=800){
+    function changeCol(color){      
+        document.getElementById('scrollxID').style.backgroundColor = color;
+    }
+}
+
+
